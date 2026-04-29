e1e018080d0b6a68ed1a1f917a01a0e71225b87a
Hood-ley <hoodleymartin.lr@gmail.com>
2026-04-29 11:57:00 +0300

    update

--- app/src/main/java/com/fredrickjasin/todo/Ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/fredrickjasin/todo/Ui/screens/home/HomeScreen.kt
@@ -1,2 +1,79 @@
 package com.fredrickjasin.todo.Ui.screens.home
 
+import android.R.attr.description
+import android.R.attr.id
+import android.R.id.title
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+@Composable
+fun HomScreen(
+    modifier: Modifier,
+    HomeViewModel: HomeViewModel = viewModel(),
+){
+    val alltodos = HomeViewModel.alltodos.collectAsState()
+    var todoid by  remember { mutableStateOf(TextFieldValue("")) }
+    var todotitle by remember { mutableStateOf(TextFieldValue("")) }
+
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        OutlinedTextField(
+            value = todoid,
+            onValueChange = { todoid = it },
+            label = { Text(text = "Todo Id") },
+            maxLines = 1
+        )
+        OutlinedTextField(
+            value = todotitle,
+            onValueChange = { todotitle = it },
+            label = { Text(text = "Todo title") },
+            minLines = 1
+        )
+
+        OutlinedButton(
+            onClick = {
+                HomeViewModel.createalltodos(
+                    id = todoid.hashCode())
+            }
+        ) {
+            Text(text = "Create Todo")
+        }
+        HorizontalDivider()
+        Text(
+            text = alltodos.value.title
+        )
+        Text(text = alltodos.value.description)
+
+
+    }
+
+
+}
+//fun createHobby(name:String, description:String): HobbyDataModel{
+//    return HobbyDataModel(name= name, description = description)
+//}
+
+
--- app/src/main/java/com/fredrickjasin/todo/Ui/screens/home/HomeViewModel.kt
+++ app/src/main/java/com/fredrickjasin/todo/Ui/screens/home/HomeViewModel.kt
@@ -1,4 +1,21 @@
 package com.fredrickjasin.todo.Ui.screens.home
 
-class HomeViewModel {
+import androidx.lifecycle.ViewModel
+import com.fredrickjasin.todo.data.models.Todo
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class HomeViewModel: ViewModel() {
+
+    //    State
+    private val _alltodos = MutableStateFlow<Todo>(Todo())
+    val alltodos = _alltodos.asStateFlow()
+
+    //    "CRUD" methods
+    fun createalltodos(id: Int, createdAt: String,title: String,description: String,media: String, isComplete: Boolean,dueDate: String ){
+        _alltodos.value = Todo(id=id, title=title, description = description)
+    }
+
+
+
 }
